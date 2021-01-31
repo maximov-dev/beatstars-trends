6988d8da8fb1ec69659ef10c5c6819085e056cdb
Yaroslav Maximov <sHarveyWilliams@gmail.com>
2021-01-31 20:19:34 +0300

    fixed date calculation

--- src/components/app.js
+++ src/components/app.js
@@ -5,15 +5,17 @@ import Header from './header';
 import DatePickerCustom from './date-picker';
 
 function App() {
-	const [date, setDate] = useState(new Date());
-	const [fullDate, setFullDate] = useState({});
-	const dataService = new DataService();
 	const parseDate = (date) => new Date(Date.parse(date));
 	const formatDate = (date) => {
+		console.log(date);
 		const parsedDate = parseDate(date);
-
-		return `${parsedDate.getFullYear()}-0${parsedDate.getDay()}-0${parsedDate.getMonth() + 1}`
+		const addZeroSymbol = (value) => (value > 9 ? `${value}` : `0${value}`);
+		console.log(parsedDate.getDay())
+		return `${parsedDate.getFullYear()}-${addZeroSymbol(parsedDate.getMonth() + 1)}-${addZeroSymbol(parsedDate.getDate())}`;
 	};
+	const [date, setDate] = useState(new Date());
+	const [fullDate, setFullDate] = useState({ startDate: formatDate(date), endDate: formatDate(date)});
+	const dataService = new DataService();
 
 	useEffect(() => {
 		const formattedDate = formatDate(date);
@@ -24,21 +26,11 @@ function App() {
 	  return (
 	<div id="app">
 	  <div class="container">
-				<div class = "row"><div class = "col-md-12">
-					<Header />
-				</div>
-			</div>
-	  		<div class = "row">
-				  <div class = "col-md-12">
-					  <DatePickerCustom date={date} setDate={setDate} />
-					</div>
-			</div>
-	  		<div class = "row"><div class = "col-md-12">
-			  <GridTrends 
+			<Header />
+			<DatePickerCustom date={date} setDate={setDate} />
+			<GridTrends 
 				getDataByDate={dataService.getDataByDate} 
 			  	date={fullDate} />
-			</div>
-		</div>
   		</div>
 		  <footer class="bg-light text-center text-lg-start">
 			<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
